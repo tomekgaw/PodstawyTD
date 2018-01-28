0665fd1f85fa1e855cfe80b804c4bd8955fd5dca
Tomek <tomekgaw@gmailc.com>
2018-01-28 23:22:41 +0100

    panda3 :/s

--- PTD5/ptd5.py
+++ PTD5/ptd5.py
@@ -3,31 +3,19 @@ import numpy as np
 import ptd4copy as ptd4
 from itertools import accumulate
 
-
-
 #######PSK
-
-sp = []
-n = np.arange(ptd4.M)
-d = np.arange(ptd4.fs)
-for b in n:
-    for s in d:
-        x = np.sin(2 * np.pi * ptd4.fn * s/ ptd4.fs)
-        sp.append(x)
-
+sp = [(np.sin(2 * np.pi * ptd4.fn * b/ ptd4.fs)) for b in range(1500)]
 psk = np.multiply(ptd4.zp,sp)
 def sumation(x):
-    n = np.arange(ptd4.M)
-    d = np.arange(ptd4.fs)
-    suma = 0
+    xd = suma = 0
     xt = []
-    for i in n:
+    for i in np.arange(ptd4.M):
         suma = 0
-        for j in d:
-            suma += x[j]
+        for j in np.arange(ptd4.fs):
+            suma += x[xd]
             xt.append(suma)
+            xd = xd + 1
     return xt
-
 
 def demod1(d,h):
     s = []
@@ -39,52 +27,95 @@ def demod1(d,h):
             s.append(0)
     return s
 
+# h1 =10
+# xt = sumation(psk)
+# ss =  demod1(xt,h1)
+# plt.figure()
+# plt.subplot(3,1,1)
+# plt.title('demodulacja sygnału PSK dla h = {0}'.format(h1))
+# plt.xlabel('t')
+# plt.ylabel('x(t)')
+# plt.plot(psk)
+# plt.subplot(3,1,2)
+# plt.xlabel('t')
+# plt.ylabel('p(t)')
+# plt.plot(xt)
+# plt.subplot(3,1,3)
+# plt.xlabel('t')
+# plt.ylabel('m(t)')
+# plt.plot(ss)
+# plt.savefig('lab5zad1PSK')
+#
+#
+# sa = [ptd4.A2 * np.sin(2 * np.pi * ptd4.fn * b / ptd4.fs) for b in range(1500)]
+#
+#
+# h2 = 35
+#
+# ask = np.multiply(ptd4.za,sp)
+# s1 = sumation(ask)
+# m2 = demod1(s1,h2)
+#
+# plt.figure()
+# plt.subplot(3,1,1)
+# plt.title('demodulacja sygnału ASK dla h = {0}'.format(h2))
+# plt.xlabel('t')
+# plt.ylabel('x(t)')
+# plt.plot(ask)
+# plt.subplot(3,1,2)
+# plt.xlabel('t')
+# plt.ylabel('p(t)')
+# plt.plot(s1)
+# plt.subplot(3,1,3)
+# plt.xlabel('t')
+# plt.ylabel('m(t)')
+# plt.plot(m2)
+# plt.savefig('lab5zad1ASK')
 
-h1 = 4
-xt = sumation(psk)
-ss =  demod1(xt,h1)
+sn1 = [np.sin(2 * np.pi * ptd4.fn1 * b / ptd4.fs) for b in range(1500)]
+sn2 = [np.sin(2 * np.pi * ptd4.fn2 * b / ptd4.fs) for b in range(1500)]
+xt1 = np.multiply(ptd4.zf,sn1)
+xt2 = np.multiply(ptd4.zf,sn2)
+px1 = sumation(xt1)
+px2 = sumation(xt2)
+
+pt = px1 + px2
+h3 = 23
+m3 = demod1(pt,h3)
+
 plt.figure()
-plt.subplot(3,1,1)
-plt.title('demodulacja sygnału PSK dla h = {0}'.format(h1))
+plt.subplot(2,1,1)
+plt.title('FSK x1(t) i px1(t)')
 plt.xlabel('t')
-plt.ylabel('x(t)')
-plt.plot(psk)
-plt.subplot(3,1,2)
+plt.ylabel('x1(t)')
+plt.plot(xt1)
+plt.subplot(2,1,2)
+plt.xlabel('t')
+plt.ylabel('px2(t)')
+plt.plot(px1)
+plt.savefig('lab5FSKxt')
+plt.figure()
+plt.subplot(2,1,1)
+plt.title('FSK x2(t) i px2(t)')
+plt.xlabel('t')
+plt.ylabel('x1(t)')
+plt.plot(xt2)
+plt.subplot(2,1,2)
+plt.xlabel('t')
+plt.ylabel('px2(t)')
+plt.plot(px2)
+plt.savefig('lab5FSKpxt')
+plt.figure()
+
+plt.subplot(2,1,1)
+plt.title('demodulacja sygnału FSK dla h = {0}'.format(h3))
 plt.xlabel('t')
 plt.ylabel('p(t)')
-plt.plot(xt)
-plt.subplot(3,1,3)
+plt.plot(pt)
+plt.subplot(2,1,2)
 plt.xlabel('t')
 plt.ylabel('m(t)')
-plt.plot(ss)
-plt.savefig('lab5zad1PSK')
+plt.plot(m3)
+plt.savefig('lab5FSKmtpt')
 
-
-sa = []
-for _ in n:
-    for s in d:
-        sa.append(ptd4.A2 * np.sin(2 * np.pi * ptd4.fn * s / ptd4.fs))
-
-
-h2 = 3
-
-ask = np.multiply(ptd4.za,sp)
-s1 = sumation(ask)
-m2 = demod1(s1,h2)
-
-plt.figure()
-plt.subplot(3,1,1)
-plt.title('demodulacja sygnału PSK dla h = {0}'.format(h2))
-plt.xlabel('t')
-plt.ylabel('x(t)')
-plt.plot(ask)
-plt.subplot(3,1,2)
-plt.xlabel('t')
-plt.ylabel('p(t)')
-plt.plot(s1)
-plt.subplot(3,1,3)
-plt.xlabel('t')
-plt.ylabel('m(t)')
-plt.plot(m2)
-plt.savefig('lab5zad1ASK')
 plt.show()
